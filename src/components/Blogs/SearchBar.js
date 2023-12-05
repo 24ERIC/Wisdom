1d04e05416622a4fcfbdf76578e2d8b8e6859417
24ERIC <60751655+24ERIC@users.noreply.github.com>
2023-12-05 12:46:53 -0500

    finish search bar frontend code

--- src/components/Blogs/SearchBar.js
+++ src/components/Blogs/SearchBar.js
@@ -1,71 +1,104 @@
-import * as React from 'react';
 import { styled, alpha } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import InputBase from '@mui/material/InputBase';
 import SearchIcon from '@mui/icons-material/Search';
+import React, { useState } from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
 
 const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(1),
-    width: 'auto',
-  },
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+        backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(1),
+        width: 'auto',
+    },
 }));
 
 const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
 }));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  width: '100%',
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    [theme.breakpoints.up('sm')]: {
-      width: '12ch',
-      '&:focus': {
-        width: '20ch',
-      },
+    color: 'inherit',
+    width: '100%',
+    '& .MuiInputBase-input': {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        transition: theme.transitions.create('width'),
+        [theme.breakpoints.up('sm')]: {
+            width: '12ch',
+            '&:focus': {
+                width: '20ch',
+            },
+        },
     },
-  },
 }));
+
 export default function SearchBar() {
+    const [open, setOpen] = useState(false);
+    const [searchResults, setSearchResults] = useState([]);
+
+    const handleSearch = (event) => {
+        event.preventDefault();
+        const results = ['Result 1', 'Result 2', 'Result 3'];
+        setSearchResults(results);
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
     return (
-      <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center', margin: 2 }}> {/* Added margin */}
-        <Search sx={{ width: '50%' }}> {/* Adjust the width as needed */}
-          <SearchIconWrapper>
-            <SearchIcon />
-          </SearchIconWrapper>
-          <StyledInputBase
-            placeholder="Search…"
-            inputProps={{ 'aria-label': 'search' }}
-            sx={{
-              '& .MuiInputBase-input': {
-                width: '100%', // Adjust initial width
-                '&:focus': {
-                  width: '100%', // Adjust width on focus
-                },
-              },
-            }}
-          />
-        </Search>
-      </Box>
+        <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center', margin: 2 }}>
+            <form onSubmit={handleSearch}>
+                <Search sx={{ width: '50%' }}>
+                    <SearchIconWrapper>
+                        <SearchIcon />
+                    </SearchIconWrapper>
+                    <StyledInputBase
+                        placeholder="Search…"
+                        inputProps={{ 'aria-label': 'search' }}
+                        sx={{
+                            '& .MuiInputBase-input': {
+                                width: '100%',
+                                '&:focus': {
+                                    width: '100%',
+                                },
+                            },
+                        }}
+                    />
+                </Search>
+            </form>
+
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>Search Results</DialogTitle>
+                <DialogContent>
+                    {searchResults.map((result, index) => (
+                        <div key={index}>{result}</div>
+                    ))}
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>Close</Button>
+                </DialogActions>
+            </Dialog>
+        </Box>
     );
-  }
-  
+}
