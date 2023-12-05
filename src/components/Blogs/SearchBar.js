0c84b73f9a54c9b2e4290a6d79811d07c0766d6a
24ERIC <60751655+24ERIC@users.noreply.github.com>
2023-12-05 12:26:56 -0500

    finish style of search in blogs

--- src/components/Blogs/SearchBar.js
+++ src/components/Blogs/SearchBar.js
@@ -1,12 +1,7 @@
 import * as React from 'react';
 import { styled, alpha } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
 import InputBase from '@mui/material/InputBase';
-import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
 
 const Search = styled('div')(({ theme }) => ({
@@ -50,40 +45,27 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
     },
   },
 }));
-
 export default function SearchBar() {
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
-          >
-            MUI
-          </Typography>
-          <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ 'aria-label': 'search' }}
-            />
-          </Search>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
-}
+    return (
+      <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center', margin: 2 }}> {/* Added margin */}
+        <Search sx={{ width: '50%' }}> {/* Adjust the width as needed */}
+          <SearchIconWrapper>
+            <SearchIcon />
+          </SearchIconWrapper>
+          <StyledInputBase
+            placeholder="Search…"
+            inputProps={{ 'aria-label': 'search' }}
+            sx={{
+              '& .MuiInputBase-input': {
+                width: '100%', // Adjust initial width
+                '&:focus': {
+                  width: '100%', // Adjust width on focus
+                },
+              },
+            }}
+          />
+        </Search>
+      </Box>
+    );
+  }
+  
