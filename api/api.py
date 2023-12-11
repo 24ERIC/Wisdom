e811c6293bcd779bb05a4017b601d813e70bedd6
24ERIC <60751655+24ERIC@users.noreply.github.com>
2023-12-11 12:21:08 -0500

    move indexing helper into api.js

--- api/api.py
+++ api/api.py
@@ -5,6 +5,9 @@ from flask_migrate import Migrate
 import json
 from difflib import get_close_matches
 from collections import Counter
+import os
+import json
+import re
 
 app = Flask(__name__, static_folder='../build', static_url_path='/')
 CORS(app, resources={r"/api/*": {"origins": "*"}})
@@ -12,6 +15,19 @@ CORS(app, resources={r"/api/*": {"origins": "*"}})
 # db = SQLAlchemy(app)
 # migrate = Migrate(app, db)
 
+#########################################################################################################
+# Table of Content
+# - 
+# - 
+# - 
+# - 
+#########################################################################################################
+
+
+
+#########################################################################################################
+# Blog Search ###########################################################################################
+#########################################################################################################
 def rank_results(query, results):
     query_terms = Counter(query.split())
     ranked_results = sorted(
@@ -55,10 +71,48 @@ def search():
         return jsonify({'message': message, 'results': results})
     
     return jsonify(results)
+#########################################################################################################
+
+
+
+#########################################################################################################
+# Blog Search Indexing, Generate/Update blog_index.json #################################################
+#########################################################################################################
+def extract_tags(content):
+    match = re.search(r'^Tags:\s*(.+)$', content, re.MULTILINE)
+    if match:
+        return [tag.strip() for tag in match.group(1).split(',')]
+    return []
+
+def index_blog(directory):
+    index = []
+    base_url = "https://24eric.github.io/Wisdom/Blog/"
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith('.md'):
+                relative_path = os.path.relpath(root, directory)
+                filepath = os.path.join(root, file)
+                with open(filepath, 'r') as md_file:
+                    content = md_file.read()
+                tags = extract_tags(content)
+                # Convert file path to URL
+                if relative_path != '.':
+                    web_path = base_url + os.path.join(relative_path, file).replace('.md', '.html').replace('\\', '/')
+                else:
+                    web_path = base_url + file.replace('.md', '.html')
+                index.append({'name': file, 'path': web_path, 'tags': tags})
+                print(f'Indexed: {file}')
+    return index
+#########################################################################################################
+
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
     app.run(debug=True)
+    
+    # Blog Search Indexing, Generate/Update blog_index.json
+    with open('api/blog_index.json', 'w') as index_file:
+        json.dump(index_blog('Blog'), index_file)
     
     
