4e525675ba14302a3d4197cd5c98c9d9ecc193c1
24ERIC <60751655+24ERIC@users.noreply.github.com>
2023-12-11 12:12:56 -0500

    finish frontend blog search, include highlight and styling

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -30,6 +30,9 @@
 // }
 
 
+
+
+// version 2
 import React, { useState } from 'react';
 import './Blogs.css'; // Assuming you have a CSS file named Blogs.css
 
@@ -44,20 +47,57 @@ export default function Blogs() {
     setResults(data);
   };
 
+  const highlightText = (text, highlight, url) => {
+    let highlightedText = text;
+    if (highlight.trim()) {
+      const regex = new RegExp(`(${highlight})`, 'gi');
+      highlightedText = text.replace(regex, '<mark>$1</mark>');
+    }
+    return `<a href="${url}" target="_blank" rel="noopener noreferrer" class="search-result-link">${highlightedText}</a>`;
+  };
+
+
   return (
+    // <div className="search-container">
+    //   <div className="search-bar">
+    //     <input type="text" placeholder="Search Query" value={query} onChange={(e) => setQuery(e.target.value)} />
+    //     <input type="text" placeholder="Tags (comma-separated)" value={tag} onChange={(e) => setTag(e.target.value)} />
+    //     <button onClick={handleSearch}>Search</button>
+    //   </div>
+    //   <ul className="results-list">
+    //     {/* {results.map(item => (
+    //       <li key={item.path}>
+    //         <a href={item.path} target="_blank" rel="noopener noreferrer">{item.name}</a> - Tags: {item.tags.join(', ')}
+    //       </li>
+    //     ))} */}
+    //     {results.map(item => (
+    //       <div key={item.path} className="search-result">
+    //         <a href={item.path} target="_blank" rel="noopener noreferrer" className="search-result-title">{item.name}</a>
+    //         <div className="search-result-url">{item.path}</div>
+    //         <div className="search-result-tags">Tags: {item.tags.join(', ')}</div>
+    //       </div>
+    //     ))}
+
+    //   </ul>
+    //   <button className="blog-button"><a href="https://24eric.github.io/Wisdom/">Blog</a></button>
+    // </div>
     <div className="search-container">
       <div className="search-bar">
         <input type="text" placeholder="Search Query" value={query} onChange={(e) => setQuery(e.target.value)} />
         <input type="text" placeholder="Tags (comma-separated)" value={tag} onChange={(e) => setTag(e.target.value)} />
         <button onClick={handleSearch}>Search</button>
       </div>
-      <ul className="results-list">
-        {results.map(item => (
-          <li key={item.path}>
-            <a href={item.path} target="_blank" rel="noopener noreferrer">{item.name}</a> - Tags: {item.tags.join(', ')}
-          </li>
-        ))}
-      </ul>
+      <table className="results-table">
+        <tbody>
+          {results.map(item => (
+            <tr key={item.path}>
+              <td dangerouslySetInnerHTML={{ __html: highlightText(item.name, query, item.path) }} />
+              <td>{item.tags.filter(t => t.includes(tag)).join(', ')}</td>
+            </tr>
+          ))}
+
+        </tbody>
+      </table>
       <button className="blog-button"><a href="https://24eric.github.io/Wisdom/">Blog</a></button>
     </div>
   );
