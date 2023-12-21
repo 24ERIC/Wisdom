9634d4fa6a2dd605924f8bb2ec1cb4172df48aca
24ERIC <60751655+24ERIC@users.noreply.github.com>
2023-12-20 22:25:31 -0500

    finish search popup window

--- src/components/Header/HeaderBarSearch.js
+++ src/components/Header/HeaderBarSearch.js
@@ -1,7 +1,9 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import { styled, alpha } from '@mui/material/styles';
 import InputBase from '@mui/material/InputBase';
 import SearchIcon from '@mui/icons-material/Search';
+import HeaderSearchPopup from './HeaderSearchPopup'; // Ensure this path is correct
+
 
 const Search = styled('div')(({ theme }) => ({
     position: 'relative',
@@ -24,36 +26,43 @@ const SearchIconWrapper = styled('div')(({ theme }) => ({
     padding: theme.spacing(0, 2),
     height: '100%',
     position: 'absolute',
-    pointerEvents: 'none',
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
+    cursor: 'pointer',
 }));
+
+
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
     color: 'inherit',
     '& .MuiInputBase-input': {
-        padding: theme.spacing(1, 1, 1, 0),
-        // vertical padding + font size from searchIcon
+        padding: theme.spacing(1),
         paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-        transition: theme.transitions.create('width'),
         width: '100%',
-        [theme.breakpoints.up('md')]: {
-            width: '20ch',
-        },
     },
 }));
 
+
 export default function HeaderBarSearch() {
+    const [open, setOpen] = React.useState(false);
+
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
     return (
-        <Search>
-            <SearchIconWrapper>
-                <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-                placeholder="Search…"
-                inputProps={{ 'aria-label': 'search' }}
-            />
-        </Search>
+        <>
+            <Search onClick={handleOpen}>
+                <SearchIconWrapper>
+                    <SearchIcon />
+                </SearchIconWrapper>
+                <StyledInputBase
+                    placeholder="Search…"
+                    inputProps={{ 'aria-label': 'search' }}
+                />
+            </Search>
+            <HeaderSearchPopup open={open} handleClose={handleClose} />
+        </>
     );
 }
+
