ab88b8fcb546f22290714ebb16799a19d1f6c805
24ERIC <60751655+24ERIC@users.noreply.github.com>
2023-12-23 22:35:23 -0500

    drop file, inifinite image preview

--- src/components/Search/DragDropFileUpload.js
+++ src/components/Search/DragDropFileUpload.js
@@ -1,115 +1,130 @@
 import { useCallback, useState } from 'react';
-import { Box, Paper, Typography, IconButton, Grid, CircularProgress } from '@mui/material';
+import { Box, Paper, Typography, IconButton, CircularProgress } from '@mui/material';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import React from 'react';
 
 function DragDropFileUpload({ onFileUpload }) {
-  const [dragOver, setDragOver] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const [imagePreview, setImagePreview] = useState(null);
+    const [dragOver, setDragOver] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [imagePreviews, setImagePreviews] = useState([]);
 
-  const handleDragOver = useCallback((event) => {
-    event.preventDefault();
-    setDragOver(true);
-  }, []);
+    const handleDragOver = useCallback((event) => {
+        event.preventDefault();
+        setDragOver(true);
+    }, []);
 
-  const handleDragLeave = useCallback((event) => {
-    event.preventDefault();
-    setDragOver(false);
-  }, []);
+    const handleDragLeave = useCallback((event) => {
+        event.preventDefault();
+        setDragOver(false);
+    }, []);
 
-  const handleDrop = useCallback(
-    (event) => {
-      event.preventDefault();
-      setDragOver(false);
-      const files = event.dataTransfer.files;
-      if (files && files[0]) {
-        handleFileChange(files[0]);
-      }
-    },
-    []
-  );
+    const handleDrop = useCallback(
+        (event) => {
+            event.preventDefault();
+            setDragOver(false);
+            const files = event.dataTransfer.files;
+            if (files) {
+                Array.from(files).forEach(file => handleFileChange(file));
+            }
+        },
+        []
+    );
 
-  const handleFileChange = (file) => {
-    setLoading(true);
-    onFileUpload(file); 
-    const reader = new FileReader();
-    reader.onloadend = () => {
-      setLoading(false);
-      setImagePreview(reader.result);
+    const handleFileChange = (file) => {
+        setLoading(true);
+        onFileUpload(file);
+        const reader = new FileReader();
+        reader.onloadend = () => {
+            setLoading(false);
+            setImagePreviews(prev => [...prev, reader.result]);
+        };
+        reader.readAsDataURL(file);
     };
-    reader.readAsDataURL(file);
-  };
 
-  const handleChange = useCallback(
-    (event) => {
-      const files = event.target.files;
-      if (files && files[0]) {
-        handleFileChange(files[0]);
-      }
-    },
-    []
-  );
+    const handleChange = useCallback(
+        (event) => {
+            const files = event.target.files;
+            if (files) {
+                Array.from(files).forEach(file => handleFileChange(file));
+            }
+        },
+        []
+    );
 
-  return (
-    <Box>
-      <Paper
-        variant="outlined"
-        onDragOver={handleDragOver}
-        onDragLeave={handleDragLeave}
-        onDrop={handleDrop}
-        style={{
-          border: dragOver ? '2px dashed #000' : '2px dashed #aaa',
-          padding: 20,
-          textAlign: 'center',
-          cursor: 'pointer',
-          background: dragOver ? '#eee' : '#fafafa',
-          position: 'relative',
-        }}
-      >
-        <input
-          accept="image/*"
-          style={{ display: 'none' }}
-          id="raised-button-file"
-          multiple
-          type="file"
-          onChange={handleChange}
-        />
-        <label htmlFor="raised-button-file">
-          <Box display="flex" flexDirection="column" alignItems="center">
-            <IconButton color="primary" aria-label="upload picture" component="span">
-              <CloudUploadIcon style={{ fontSize: 60 }} />
-            </IconButton>
-            <Typography>Drag and drop files here or click to select files</Typography>
-          </Box>
-        </label>
-        {loading && (
-          <CircularProgress
-            size={24}
-            style={{
-              position: 'absolute',
-              top: '50%',
-              left: '50%',
-              marginTop: '-12px',
-              marginLeft: '-12px',
-            }}
-          />
-        )}
-      </Paper>
-      {imagePreview && (
-        <Grid container justifyContent="center" style={{ marginTop: 16 }}>
-          <Grid item xs={12} sm={6} md={4}>
+    return (
+        <Box>
+            <Paper
+                variant="outlined"
+                onDragOver={handleDragOver}
+                onDragLeave={handleDragLeave}
+                onDrop={handleDrop}
+                style={{
+                    border: dragOver ? '2px dashed #000' : '2px dashed #aaa',
+                    padding: 20,
+                    textAlign: 'center',
+                    cursor: 'pointer',
+                    background: dragOver ? '#eee' : '#fafafa',
+                    position: 'relative',
+                }}
+            >
+                <input
+                    accept="image/*"
+                    style={{ display: 'none' }}
+                    id="raised-button-file"
+                    multiple
+                    type="file"
+                    onChange={handleChange}
+                />
+                <label htmlFor="raised-button-file">
+                    <Box display="flex" flexDirection="column" alignItems="center">
+                        <IconButton color="primary" aria-label="upload picture" component="span">
+                            <CloudUploadIcon style={{ fontSize: 60 }} />
+                        </IconButton>
+                        <Typography>Drag and drop files here or click to select files</Typography>
+                    </Box>
+                </label>
+                {loading && (
+                    <CircularProgress
+                        size={24}
+                        style={{
+                            position: 'absolute',
+                            top: '50%',
+                            left: '50%',
+                            marginTop: '-12px',
+                            marginLeft: '-12px',
+                        }}
+                    />
+                )}
+            </Paper>
             <Box
-              component="img"
-              src={imagePreview}
-              alt="Image Preview"
-              sx={{ width: '100%', height: 'auto' }}
-            />
-          </Grid>
-        </Grid>
-      )}
-    </Box>
-  );
+                sx={{
+                    display: 'flex',
+                    overflowX: 'auto',
+                    marginTop: 2,
+                    padding: 1,
+                    '&::-webkit-scrollbar': {
+                        height: '10px'
+                    },
+                    '&::-webkit-scrollbar-thumb': {
+                        background: '#888'
+                    },
+                    '&::-webkit-scrollbar-thumb:hover': {
+                        background: '#555'
+                    }
+                }}
+            >
+                {imagePreviews.map((image, index) => (
+                    <Box
+                        key={index}
+                        component="img"
+                        src={image}
+                        alt={`Image Preview ${index + 1}`}
+                        sx={{ width: 'auto', height: 100, marginRight: 2 }}
+                    />
+                ))}
+            </Box>
+        </Box>
+    );
 }
 
 export default DragDropFileUpload;
