a219dccf9b3a431028553fd92eb535aa53b390c2
24ERIC <60751655+24ERIC@users.noreply.github.com>
2023-12-24 12:13:49 -0500

    drop update in search page has close icon for each preview image

--- src/components/Search/DragDropFileUpload.js
+++ src/components/Search/DragDropFileUpload.js
@@ -1,5 +1,6 @@
 import { useCallback, useState } from 'react';
 import { Box, Paper, Typography, IconButton, CircularProgress } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import React from 'react';
 
@@ -12,6 +13,10 @@ function DragDropFileUpload({ onFileUpload }) {
         event.preventDefault();
         setDragOver(true);
     }, []);
+    const removeImagePreview = (indexToRemove) => {
+        setImagePreviews(prev => prev.filter((_, index) => index !== indexToRemove));
+    };
+
 
     const handleDragLeave = useCallback((event) => {
         event.preventDefault();
@@ -114,13 +119,27 @@ function DragDropFileUpload({ onFileUpload }) {
                 }}
             >
                 {imagePreviews.map((image, index) => (
-                    <Box
-                        key={index}
-                        component="img"
-                        src={image}
-                        alt={`Image Preview ${index + 1}`}
-                        sx={{ width: 'auto', height: 100, marginRight: 2 }}
-                    />
+                    <Box key={index} position="relative">
+                        <IconButton
+                            onClick={() => removeImagePreview(index)}
+                            style={{
+                                position: 'absolute',
+                                right: 0,
+                                top: 0,
+                                color: 'white',
+                                background: 'rgba(0, 0, 0, 0.6)',
+                                borderRadius: '50%',
+                            }}
+                        >
+                            <CloseIcon />
+                        </IconButton>
+                        <Box
+                            component="img"
+                            src={image}
+                            alt={`Image Preview ${index + 1}`}
+                            sx={{ width: 'auto', height: 100, marginRight: 2 }}
+                        />
+                    </Box>
                 ))}
             </Box>
         </Box>
