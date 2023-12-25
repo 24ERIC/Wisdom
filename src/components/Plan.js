5174c3a01a056d205f19d071a7edca1c8a39e1eb
24ERIC <60751655+24ERIC@users.noreply.github.com>
2023-12-24 20:15:17 -0500

    reset plan page

--- src/components/Plan.js
+++ src/components/Plan.js
@@ -1,15 +1,8 @@
 import React from 'react';
-import DragDropFileUpload from './Search/DragDropFileUpload';
 
 function Plan() {
-  const handleFileUpload = (file) => {
-    console.log(file);
-  };
-
   return (
-    <div style={{ padding: 50 }}>
-      <DragDropFileUpload onFileUpload={handleFileUpload} />
-    </div>
+    <></>
   );
 }
 
