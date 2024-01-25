d5b4d220a8527bb002fc0a91ecb8c1a927df9e1e
24ERIC <60751655+24ERIC@users.noreply.github.com>
2024-01-25 13:54:28 -0500

    update

--- api/api.py
+++ api/api.py
@@ -21,213 +21,71 @@ class Block(db.Model):
     parent_id = db.Column(db.Integer)
     child_id = db.Column(db.Integer)
     content = db.Column(db.Text)
-    type = db.Column(Enum('page', 'unordered-list-item', 'ordered-list-item', 'header-one', 'header-two', 'header-three', 'header-four', 'header-five', 'header-six', 'blockquote', 'code-block', 'atomic', name='valid_block_type')) 
-        # default null = plain text
-        # page = a link (draft can handle link, Editor, EditorState, RichUtils, Entity, ContentState )
-        # todo-list-item (draft no, )
-        # toggle list = 
-    media_data = db.Column(db.BLOB)
+    type = db.Column(Enum('page', 'page-link', 'unordered-list-item', 'ordered-list-item', 'header-one', 'header-two', 'header-three', 'header-four', 'header-five', 'header-six', 'blockquote', 'code-block', 'atomic', name='valid_block_type')) 
+    media_data = db.Column(db.Text)
     depth = db.Column(db.Integer, default=0)
+    
+    @classmethod
+    def get_root_blocks(cls): 
+        return cls.query.filter(cls.root_page_id.is_(None)).all()
+    
+    def update_content(self, new_content):
+        self.content = new_content
+        db.session.commit()
+        
+    def add_child(self, child_block):
+        child_block.parent_id = self.id
+        db.session.add(child_block)
+        db.session.commit()
+        
+    def delete_child(self, child_block_id):
+        child_block = Block.query.get(child_block_id)
+        if child_block and child_block.parent_id == self.id:
+            db.session.delete(child_block)
+            db.session.commit()
+            
+    def get_children(self):
+        return Block.query.filter_by(parent_id=self.id).all()
+    
+    def set_type(self, new_type):
+        self.type = new_type
+        db.session.commit()
+        
+    def get_parent(self):
+        return Block.query.get(self.parent_id) if self.parent_id else None
+
+    def delete_recursive(self):
+        for child in self.get_children():
+            child.delete_recursive()
+        db.session.delete(self)
+        db.session.commit()
+
+    def split_block(self, split_index):
+        new_block = Block(
+            content=self.content[split_index:],
+            type=self.type
+        )
+        self.content = self.content[:split_index]
+        self.add_child(new_block)
+        return new_block
 
 
-# problem
-# custom todo-list, toggle-list, page-link-redirect
-# custom bullet-point inside toggle-list
+
 
 
     
+    
+@app.route('/test', methods=['GET'])
+def test():
+    
 
-
-
-
-
-
-class Page(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    block_id = db.Column(db.Integer)
-    title = db.Column(db.String)
-    star = db.Column(db.Integer) # 0 is no start, 1 is stared
 
 @app.route('/blocks/<int:current_block_id>', methods=['POST'])
-def split_block(current_block_id):
-    data = request.json
-    curr_content = data.get('currContent')
-    new_content = data.get('newContent')
-    original_block = Block.query.get(current_block_id)
-    if not original_block:
-        return jsonify({"error": "Block not found"}), 404
-    original_block.content = curr_content
-    new_block = Block(
-        parent_id=current_block_id,
-        content=new_content,
-        type=original_block.type,
-        list_child_id=original_block.list_child_id
-    )
-    db.session.add(new_block)
-    db.session.commit()
-    original_block.list_child_id = None
-    if original_block.child_id:
-        existing_child_block = Block.query.get(original_block.child_id)
-        if existing_child_block:
-            existing_child_block.parent_id = new_block.id
-            new_block.child_id = original_block.child_id
-    original_block.child_id = new_block.id
-    db.session.commit()
-    return jsonify(block_id=new_block.id), 201
-
 @app.route('/blocks/<int:block_id>', methods=['PUT'])
-def handle_block_put(block_id):
-    block = Block.query.get(block_id)
-    if not block:
-        abort(404)
-    if request.method == 'PUT':
-        data = request.json
-        print(data)
-        block.content = data.get('content')
-        db.session.commit()
-        return jsonify(block_id=block.id), 200
-    elif request.method == 'DELETE':
-        delete_recursive(block_id)
-        db.session.commit()
-        return '', 204
-
 @app.route('/blocks/<int:block_id>/single', methods=['DELETE'])
-def handle_block_delete_single(block_id):
-    # print(f"{orange_text}{larger_header}page --- parent/child/list --- {page.id}, {page.block_id},{reset_format}")
-    curr_block = Block.query.get(block_id)
-    # print(f"{orange_text}{larger_header}page --- parent/child/list --- {page.id}, {page.block_id},{reset_format}")
-    page = Page.query.filter_by(block_id=block_id).first()
-    
-    if curr_block.parent_id is None and curr_block.list_child_id:
-        print(f"{orange_text}{larger_header} Enter first if --- {reset_format}")
-        child_1_block = Block.query.get(curr_block.list_child_id)
-        curr = Block.query.get(curr_block.list_child_id)
-        while curr.child_id:
-            curr = Block.query.get(curr.child_id)
-        child_last_block = curr
-        
-        if curr_block.child_id:
-            child_last_block.child_id = curr_block.child_id
-            Block.query.get(curr_block.child_id).parent_id = child_last_block.id
-            
-        page.block_id = child_1_block.id
-        child_1_block.parent_id = None
-        
-    elif curr_block.parent_id is None and curr_block.child_id:
-        page.block_id = curr_block.child_id
-        Block.query.get(curr_block.child_id).parent_id = None
-    elif curr_block.parent_id is None and curr_block.child_id is None:
-        # If there's no child, create a new block to replace the deleted root block
-        new_block = Block()
-        db.session.add(new_block)
-        db.session.flush()  # To get the new block's ID
-        page.block_id = new_block.id
-
-    elif curr_block.list_child_id is None and curr_block.child_id: # no subitem, has below item ✅
-        parent_block = Block.query.get(curr_block.parent_id)
-        parent_block.child_id = curr_block.child_id
-        
-    elif curr_block.list_child_id is None and curr_block.child_id is None: # no subitem, no below item ✅
-        parent_block = Block.query.get(curr_block.parent_id)
-        parent_block.child_id = ""
-    
-    elif curr_block.list_child_id: # has subitem
-        # init
-        parent_block = Block.query.get(curr_block.parent_id)
-        child_1_block = Block.query.get(curr_block.list_child_id)
-        curr = Block.query.get(curr_block.list_child_id)
-        while curr.child_id:
-            curr = Block.query.get(curr.child_id)
-        child_last_block = curr
-        
-        # different situation
-        if parent_block.list_child_id == curr_block.id: # parent is left
-            parent_block.list_child_id = child_1_block.id
-            
-        if parent_block.child_id == curr_block.id: # parent same 
-            parent_block.child_id = child_1_block.id
-            
-        if curr_block.child_id:
-            child_last_block.child_id = curr_block.child_id
-            Block.query.get(curr_block.child_id).parent_id = child_last_block.id
-            
-    db.session.delete(curr_block)
-    db.session.commit()
-    return '', 204
-
-
-
-
-
 @app.route('/blocks/<int:block_id>/children', methods=['DELETE'])
-def handle_block_delete_children(block_id):
-    delete_recursive(block_id)
-    db.session.commit()
-    return '', 204
-
-
 @app.route('/pages', methods=['POST'])
-def create_page():
-    data = request.json
-    new_page = Page(
-        block_id=data.get('block_id'),
-        title=data.get('title')
-    )
-    db.session.add(new_page)
-    db.session.commit()
-    return jsonify(page_id=new_page.id), 201
-
-
-
-def delete_recursive(block_id):
-    block = Block.query.get(block_id)
-    if block.child_id:
-        delete_recursive(block.child_id)
-    if block.list_child_id:
-        delete_recursive(block.list_child_id)
-    db.session.delete(block)
-
 @app.route('/pages/<int:page_id>', methods=['GET', 'PUT', 'DELETE'])
-def handle_page_put_delete(page_id):
-    page = Page.query.get(page_id)
-    if not page:
-        abort(404)
-    if request.method == 'GET':
-        def get_block_children(block_id):
-            children = []
-            while block_id:
-                child_block = Block.query.get(block_id)
-                if child_block is None:
-                    break
-                block_data = {
-                    'block_id': child_block.id,
-                    'block_content': child_block.content,
-                    'block_type': child_block.type,
-                    'block_meta': child_block.meta,
-                    'children': []
-                }
-                if child_block.list_child_id:
-                    block_data['children'] = get_block_children(child_block.list_child_id)
-                children.append(block_data)
-                block_id = child_block.child_id
-            return children
-        page_data = {
-            'page_id': page.id,
-            'page_title': page.title,
-            'blocks': get_block_children(page.block_id)
-        }
-        return jsonify(page_data)
-    elif request.method == 'PUT':
-        data = request.json
-        page.title = data.get('title')
-        db.session.commit()
-        return jsonify(page_id=page.id), 200
-    elif request.method == 'DELETE':
-        if page.block_id:
-            delete_recursive(page.block_id)
-        db.session.delete(page)
-        db.session.commit()
-        return '', 204
 
 if __name__ == '__main__':
     with app.app_context():
